3a01447e4645c7f084c19e74e4d23f2872a32442
Winnydapo <144258773+Winnydapo@users.noreply.github.com>
2023-09-06 16:07:38 +0000

    Pending changes exported from your codespace

---
+++ sqrRt/SquareRoot/src/CharAt.java
@@ -0,0 +1,19 @@
+
+
+public class CharAt {
+	find sentence = new find("sdjfkljklsvlnkldklsdfl","s");
+
+}
+class find
+{
+public String a;
+public String[] words = new String[1];
+
+	public find(String a, String target)
+	{
+			a = this.a;
+			
+
+	}
+
+}
