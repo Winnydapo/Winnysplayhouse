e6a047c410dbb5ec3ac5a4f40358cbe6a85670f0
Nolan Tran <72712954+ArvalCode@users.noreply.github.com>
2023-09-07 16:13:18 +0000

    committing

--- sqrRt/SquareRoot/src/main.java
+++ sqrRt/SquareRoot/src/main.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        int hi[] = {1,2,3};
+
         System.out.println("Hello, World!");
     }
 }
---
+++ sqrRt/SquareRoot/src/toString.java
@@ -0,0 +1,8 @@
+public class toString() {
+
+    public String toString(int[] ho) {
+        for (int i=0;i<ho.length;i++) {
+            
+        }
+    }
+}
